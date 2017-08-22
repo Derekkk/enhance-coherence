eba0b3e2039c9aa3ce65ffea796db990e8104c0b
WU Yuxiang <ywubw@qycpu10.cse.ust.hk>
2017-08-23 00:17:46 +0800

    Added class SentencePairBatcher

--- src/utils/batch_reader.py
+++ src/utils/batch_reader.py
@@ -58,7 +58,7 @@ class ExtractiveBatcher(object):
     Args:
       data_path: tf.Example filepattern.
       enc_vocab: Encoder vocabulary.
-      hps: Seq2SeqAttention model hyperparameters.
+      hps: model hyperparameters.
       bucketing: Whether bucket inputs of similar length into the same batch.
       truncate_input: Whether to truncate input that is too long. Alternative is
         to discard such examples.
@@ -291,3 +291,135 @@ class ExtractiveBatcher(object):
           bucketing_threads[-1].daemon = True
           bucketing_threads[-1].start()
       self._bucketing_threads = bucketing_threads
+
+
+class SentencePairBatcher(ExtractiveBatcher):
+  """Batch reader for sequence matching model."""
+
+  def _FillInputQueue(self, data_path):
+    """Fill input queue with ExtractiveSample."""
+    hps = self._hps
+    enc_vocab = self._enc_vocab
+    enc_pad_id = enc_vocab.pad_id
+    enc_empty_sent = [enc_pad_id] * hps.num_words_sent
+    rel_pos_max_float = float(hps.rel_pos_max_idx - 1)
+
+    data_generator = self._DataGenerator(data_path, self._num_epochs)
+
+    # pdb.set_trace()
+    for data_sample in data_generator:
+      document = data_sample.document
+      extract_ids = data_sample.extract_ids
+
+      # Content as enc_input
+      enc_input = [enc_vocab.GetIds(s) for s in document]
+
+      # Filter out too-short input
+      if len(enc_input) < hps.min_num_input_sents:
+        continue
+
+      if self._truncate_input:
+        enc_input = [
+            s[:hps.num_words_sent] for s in enc_input[:hps.num_sentences]
+        ]
+      else:
+        if len(enc_input) > hps.num_sentences:
+          continue  # throw away too long inputs
+
+      # Now enc_input should fit in 2-D matrix [num_sentences, num_words_sent]
+      enc_sent_len = [len(s) for s in enc_input]
+      enc_doc_len = len(enc_input)
+
+      # Pad enc_input if necessary
+      padded_enc_input = [
+          s + [enc_pad_id] * (hps.num_words_sent - l)
+          for s, l in zip(enc_input, enc_sent_len)
+      ]
+      padded_enc_input += [enc_empty_sent] * (hps.num_sentences - enc_doc_len)
+      np_enc_input = np.array(padded_enc_input, dtype=np.int32)
+
+      # Compute the relative position. 0 is reserved for padding.
+      rel_pos_coef = rel_pos_max_float / enc_doc_len
+      sent_rel_pos = [int(i * rel_pos_coef) + 1 for i in range(enc_doc_len)]
+
+      # Pad the input lengths and positions
+      pad_enc_sent_len = enc_sent_len + [0] * (hps.num_sentences - enc_doc_len)
+      pad_rel_pos = sent_rel_pos + [0] * (hps.num_sentences - enc_doc_len)
+      np_enc_sent_len = np.array(pad_enc_sent_len, dtype=np.int32)
+      np_rel_pos = np.array(pad_rel_pos, dtype=np.int32)
+
+      # Skip those with no extractive summaries
+      if len(extract_ids) == 0:
+        continue
+
+      np_target = np.zeros([hps.num_sentences], dtype=np.int32)
+      for i in extract_ids:
+        if i < hps.num_sentences:
+          np_target[i] = 1
+
+      if hps.trg_weight_norm > 0:
+        counts = data_sample.extract_counts
+        total_count = float(sum(counts))
+        weight_norm = hps.trg_weight_norm / (total_count + 0.01)
+        weights = [weight_norm * c for c in counts]  # normalize the weights
+
+        np_weights = np.ones([hps.num_sentences], dtype=np.float32)
+        for i, w in zip(extract_ids, weights):
+          if i < hps.num_sentences:
+            np_weights[i] = w
+      else:
+        np_weights = np.ones([hps.num_sentences], dtype=np.float32)
+
+      try:
+        summary = data_sample.summary
+      except:
+        summary = [""]
+      sample = ExtractiveSample(np_enc_input, enc_doc_len, np_enc_sent_len,
+                                np_rel_pos, np_target, np_weights, document,
+                                summary)
+      self._sample_queue.put(sample)
+
+  def _FillBucketInputQueue(self):
+    """Fill bucketed batches into the bucket_input_queue."""
+    hps = self._hps
+    while True:
+      samples = []
+      for _ in xrange(hps.batch_size * BUCKET_NUM_BATCH):
+        samples.append(self._sample_queue.get())
+
+      if self._bucketing:
+        samples = sorted(samples, key=lambda inp: inp.enc_doc_len)
+
+      batches = []
+      for i in xrange(0, len(samples), hps.batch_size):
+        batches.append(samples[i:i + hps.batch_size])
+
+      if self._shuffle_batches:
+        shuffle(batches)
+
+      for b in batches:
+        self._batch_queue.put(self._PackBatch(b))
+
+  def _PackBatch(self, batch):
+    """ Pack the batch into numpy arrays.
+
+    Returns:
+        model_batch: ExtractiveBatch
+    """
+    hps = self._hps
+    field_lists = [[], [], [], [], [], []]
+    origin_inputs, origin_outputs = [], []
+
+    for ex in batch:
+      for i in range(6):
+        field_lists[i].append(ex[i])
+      origin_inputs.append(ex.origin_input)
+      origin_outputs.append(ex.origin_output)
+
+    stacked_fields = [np.stack(field, axis=0) for field in field_lists]
+
+    return ExtractiveBatch(stacked_fields[0], stacked_fields[1],
+                           stacked_fields[2], stacked_fields[3],
+                           stacked_fields[4], stacked_fields[5],\
+                           (origin_inputs, origin_outputs))
+
